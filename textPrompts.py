875a9769ca951d2b565325880bbaa1a6b4ea30f1
AOSEI1001 <105248308+AOSEI1001@users.noreply.github.com>
2022-11-12 15:52:28 -0500

    time trigger

--- textPrompts.py
+++ textPrompts.py
@@ -1,10 +1,13 @@
 import datetime
+import time as t
 
 def dailyRoutine():
-    print('hello')
-    print(datetime.datetime.now())
-
-#question types ratings, reminder, yn
+    while 1:
+        time = str(datetime.datetime.now())[10:15]
+        checkTime(time)
+        t.sleep(20)
+        
+        #question types ratings, reminder, yn
 def responder(qtype, response):
     if qtype == 'rating':
         print(response)
@@ -12,5 +15,22 @@ def responder(qtype, response):
         print(response)
     if qtype == 'yn':
         print(response)
+    
+def checkTime(time):
+    if time == "09:0":
+        print(time)
+    elif time == "01:0":
+        print(time)
+    elif  time == "18:0":
+        print(time)
+    elif time == "22:0":
+        print(time)
+    elif time == "15:5":
+        print(time)
+        print("hello")
+    else:
+        print(time)
+        print("hi")
+    
 
 

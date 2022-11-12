24317b675431e5f0b25c9a1ab3be4b05dd2d60e7
jordanlouie1 <jordanlouie.1@gmail.com>
2022-11-12 14:21:20 -0500

    Initialize

---
+++ main.py
@@ -0,0 +1,8 @@
+import textPrompts as tp
+
+def main():
+    print("Hi!")
+    tp.dailyRoutine
+
+if __name__ == 'main':
+    main()
---
+++ textPrompts.py
@@ -0,0 +1,16 @@
+import datetime
+
+def dailyRoutine():
+    print('hello world')
+    print(datetime.time)
+
+#question types ratings, reminder, yn
+def responder(qtype, response):
+    if qtype == 'rating':
+        print(response)
+    if qtype == 'dialog':
+        print(response)
+    if qtype == 'yn':
+        print(response)
+
+
